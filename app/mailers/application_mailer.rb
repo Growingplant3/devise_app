2df254d00f7f95e152c94ff77ae706d4378a30bc
下田　佳紀 <y.s@platinum.local>
2020-12-22 01:04:05 +0900

    sendgridの環境設定

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,7 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
-  layout 'mailer'
+  def contact_mail(contact)
+    @contact = contact
+    # 下記を確認
+    mail to: "cake.underfield@gmail.com", subject: "お問い合わせの確認メール"
+  end
 end
